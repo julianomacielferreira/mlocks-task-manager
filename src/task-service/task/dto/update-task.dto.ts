0d819d5ee93782b963ce13e6272dbf0893a5ff83
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2026-02-10 22:11:56 -0300

    Create task DTOs.

---
+++ src/task-service/task/dto/update-task.dto.ts
@@ -0,0 +1,43 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2026 Juliano Maciel.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import { PartialType } from '@nestjs/mapped-types'; // Or '@nestjs/swagger' for newer versions
+import { CreateTaskDTO } from './create-task.dto';
+import { IsOptional, IsString, IsEnum } from 'class-validator';
+import { TaskStatus } from '../task.entity';
+
+// PartialType makes all fields of CreateTaskDTO optional
+export class UpdateTaskDTO extends PartialType(CreateTaskDTO) {
+
+    @IsString()
+    @IsOptional() // Explicitly mark as optional, though PartialType handles this too
+    title?: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsEnum(TaskStatus, { message: 'Status must be either OPEN, IN_PROGRESS, or DONE.' })
+    @IsOptional()
+    status?: TaskStatus;
+}
